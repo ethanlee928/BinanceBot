a281e91c0e629de465f83039dd1f0750a9a75ff2
ethanlee928 <ethan2000.el@gmail.com>
2022-06-13 13:17:43 +0000

    added socket utils

--- binance/test_socket.py
+++ binance/test_socket.py
@@ -2,23 +2,37 @@ import asyncio
 from binance import AsyncClient, BinanceSocketManager
 from binance.enums import *
 
+from multiprocessing import Process
 
-async def main():
+import time
+
+async def main(coinPair):
     client = await AsyncClient.create()
     bm = BinanceSocketManager(client)
     # start any sockets here, i.e a trade socket
     # ts = bm.trade_socket('BNBBTC')
-    ks = bm.kline_socket('ETHUSDT')
+    ks = bm.kline_socket(coinPair)
     # then start receiving messages
+    startTime = time.time()
+    now = time.time()
     async with ks as tscm:
-        while True:
+        while (now - startTime) < 20:
             res = await tscm.recv()
-            print(res)
-
-
+            now = time.time()
+            print(coinPair, res)
+    
     await client.close_connection()
 
-if __name__ == "__main__":
+def start(loop, coinPair):
+    loop.run_until_complete(main(coinPair))
 
+if __name__ == "__main__":
+    
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    t = Process(target=start, args=(loop, 'ETHUSDT',))
+    t2 = Process(target=start, args=(loop, 'BTCUSDT',))
+    t3 = Process(target=start, args=(loop, 'ENSUSDT',))
+    t.start()
+    t2.start()
+    t.join()
+    t2.join()
