d2ddc7fedac6e7f893ad9078a85739016b192bf3
Ubuntu <ubuntu@ip-172-26-1-9.ap-southeast-1.compute.internal>
2022-09-09 13:47:38 +0000

    standardize format

--- binance/binancebot.py
+++ binance/binancebot.py
@@ -14,91 +14,95 @@ from binance.client import Client
 from utils.mqtt import Subscriber, Publisher
 from utils.logger import get_logger
 
-class BinanceBot(Subscriber):
 
+class BinanceBot(Subscriber):
     def __init__(self, broker, port, topic, client_id, dataDir, timeLimit):
         super().__init__(broker, port, topic, client_id)
-        self.key = os.getenv('BINANCE_KEY')
-        self.secret = os.getenv('BINANCE_SECRET')
-        self.binanceClient = Client(self.key, self.secret) 
+        self.key = os.getenv("BINANCE_KEY")
+        self.secret = os.getenv("BINANCE_SECRET")
+        self.binanceClient = Client(self.key, self.secret)
 
-        self.slackToken = os.getenv('SLACK_TOKEN')
+        self.slackToken = os.getenv("SLACK_TOKEN")
 
         self.lastShow = None
         self.timeLimit = timeLimit
 
         self.dataDir = dataDir
 
-        self.logger = get_logger(name='BBOT')
+        self.logger = get_logger(name="BBOT")
 
         self.subThread = Thread(target=self.start_subscribe)
-        self.subThread.start() 
-        
+        self.subThread.start()
+
     def on_message(self, client, userdata, msg):
         message = msg.payload.decode()
         message = json.loads(message)
-        if message['type'] == 'message':
+        if message["type"] == "message":
             self.logger.info("message from MQTT: " % (message["message"]))
             return
 
-        if message['type'] == 'chart':
+        if message["type"] == "chart":
             self.handleKline(message=message)
             return
-        
-        if message['type'] == 'price':
+
+        if message["type"] == "price":
             self.handlePrice(message=message)
-            return 
-        
-        if message['type'] == 'info':
+            return
+
+        if message["type"] == "info":
             self.handleInfo(message=message)
             return
-        
-        if message['type'] == 'ping':
+
+        if message["type"] == "ping":
             status = self.pingServer()
             channelID = message["channelID"]
             if status:
-                self.sendMsg(msg='Binance server normal', channel=channelID)
+                self.sendMsg(msg="Binance server normal", channel=channelID)
             else:
-                self.sendMsg(msg='Binance server under maintenance', channel=channelID)
+                self.sendMsg(msg="Binance server under maintenance", channel=channelID)
             return
-
 
     def handleKline(self, message):
         now = datetime.now()
-        if self.lastShow is None or (now - self.lastShow).total_seconds() > self.timeLimit:
+        if (
+            self.lastShow is None
+            or (now - self.lastShow).total_seconds() > self.timeLimit
+        ):
             try:
-                coinPair = message['coinPair']
-                interval = message['interval']
-                startDate = message['startDate']
-                channelID = message['channelID']
+                coinPair = message["coinPair"]
+                interval = message["interval"]
+                startDate = message["startDate"]
+                channelID = message["channelID"]
                 kline = self.getKlines(coinPair, interval, startDate)
-                
-                savePath = self.plotKlines(kline=kline, type='candle', coinPair=coinPair, interval=interval)
+
+                savePath = self.plotKlines(
+                    kline=kline, type="candle", coinPair=coinPair, interval=interval
+                )
                 if savePath is None:
                     self.logger.error("Cannot plot klines")
                     return
 
                 self.sendKlines(imgpath=savePath, channel=channelID)
                 self.lastShow = datetime.now()
-            
+
             except Exception as err:
                 self.logger.error("Kline component error: %s" % err)
 
     def handlePrice(self, message):
         try:
-            coinPair = message['coinPair']
-            channelID = message['channelID']
+            coinPair = message["coinPair"]
+            channelID = message["channelID"]
             mins, price = self.getCoinPrice(coinPair=coinPair)
-            message = f'[{coinPair}] {mins}m average: {price}'
+            message = f"[{coinPair}] {mins}m average: {price}"
             self.sendMsg(msg=message, channel=channelID)
         except Exception as err:
             self.logger.error("Coinpair price component error: %s" % err)
 
     def handleInfo(self, message):
         try:
-            coinPair = message['coinPair']
-            interval = message['interval']
-            channelID = message['channelID']
+            coinPair = message["coinPair"]
+            interval = message["interval"]
+            channelID = message["channelID"]
             info = self.getInfo(coinPair=coinPair, interval=interval)
             message = f'[{coinPair}] Open Time: {info["openTime"]}\tClose Time: {info["closeTime"]}\nOpen Price: {info["openPrice"]}\nCurrent Price: {info["closePrice"]}\nHigh: {info["high"]}\nVolume: {info["volume"]}\nQuote Volume: {info["quoteVolume"]}'
             self.sendMsg(msg=message, channel=channelID)
@@ -114,73 +118,80 @@ class BinanceBot(Subscriber):
     def getPrice(self):
         prices = self.binanceClient.get_all_tickers()
         return prices
-    
+
     def getInfo(self, coinPair, interval):
         candle = self.binanceClient.get_klines(symbol=coinPair, interval=interval)[-1]
         info = {
-            "openTime": datetime.fromtimestamp(candle[0] / 1000).strftime("%Y-%m-%d %H:%M:%S"),
+            "openTime": datetime.fromtimestamp(candle[0] / 1000).strftime(
+                "%Y-%m-%d %H:%M:%S"
+            ),
             "openPrice": candle[1],
             "high": candle[2],
             "low": candle[3],
             "closePrice": candle[4],
             "volume": candle[5],
-            "closeTime": datetime.fromtimestamp(candle[6] / 1000).strftime("%Y-%m-%d %H:%M:%S"),
-            "quoteVolume": candle[7]
+            "closeTime": datetime.fromtimestamp(candle[6] / 1000).strftime(
+                "%Y-%m-%d %H:%M:%S"
+            ),
+            "quoteVolume": candle[7],
         }
         return info
-    
+
     def getCoinPrice(self, coinPair):
         avg_price = self.binanceClient.get_avg_price(symbol=coinPair)
-        mins = avg_price['mins']
-        price = avg_price['price']
+        mins = avg_price["mins"]
+        price = avg_price["price"]
         return mins, price
 
     def getKlines(self, coin_pair, interval, start_time):
-        klinesBinance = self.binanceClient.get_historical_klines(coin_pair, interval, start_time)
+        klinesBinance = self.binanceClient.get_historical_klines(
+            coin_pair, interval, start_time
+        )
         klines = []
         for item in klinesBinance:
-            openTime = datetime.fromtimestamp(item[0] / 1000).strftime("%Y-%m-%d %H:%M:%S")
+            openTime = datetime.fromtimestamp(item[0] / 1000).strftime(
+                "%Y-%m-%d %H:%M:%S"
+            )
             openPirce = item[1]
             high = item[2]
             low = item[3]
             closePrice = item[4]
             klines.append([openTime, openPirce, high, low, closePrice])
 
-        df = pd.DataFrame(klines, columns=['date', 'open', 'high', 'low', 'close'])
-        df.set_index(pd.DatetimeIndex(df['date']), inplace=True)
-        df.drop(['date'], inplace=True, axis=1)
+        df = pd.DataFrame(klines, columns=["date", "open", "high", "low", "close"])
+        df.set_index(pd.DatetimeIndex(df["date"]), inplace=True)
+        df.drop(["date"], inplace=True, axis=1)
         df = df.astype(float)
         return df
-    
+
     def sendMsg(self, msg, channel):
-        api = 'https://slack.com/api/chat.postMessage'
-        token = 'Bearer ' + self.slackToken
-        headers = {'Authorization': token}
-        data = {'channel': channel, 'text': msg}
+        api = "https://slack.com/api/chat.postMessage"
+        token = "Bearer " + self.slackToken
+        headers = {"Authorization": token}
+        data = {"channel": channel, "text": msg}
         res = requests.post(api, headers=headers, data=data)
-        self.logger.info("Message sent with response code: %s" %res.status_code)
+        self.logger.info("Message sent with response code: %s" % res.status_code)
         return res.status_code
 
     def sendKlines(self, imgpath, channel):
-        api = 'https://slack.com/api/files.upload'
-        token = 'Bearer ' + self.slackToken
-        headers = {'Authorization': token}
-        data = {'channels': channel}
-        files = {'file': open(imgpath, 'rb')}
+        api = "https://slack.com/api/files.upload"
+        token = "Bearer " + self.slackToken
+        headers = {"Authorization": token}
+        data = {"channels": channel}
+        files = {"file": open(imgpath, "rb")}
 
         res = requests.post(api, headers=headers, data=data, files=files)
-        self.logger.info("Klines sent with response code: %s" %res.status_code)
+        self.logger.info("Klines sent with response code: %s" % res.status_code)
         return res.status_code
-        
-    
+
     def plotKlines(self, kline, type, coinPair, interval):
-        title = f'{coinPair}-{interval}'
+        title = f"{coinPair}-{interval}"
         now = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-        saveDir = f'{self.dataDir}/{coinPair}/'
+        saveDir = f"{self.dataDir}/{coinPair}/"
         isExist = os.path.exists(saveDir)
         if not isExist:
             os.makedirs(saveDir)
-        savePath = f'{saveDir}/{now}.png'
+        savePath = f"{saveDir}/{now}.png"
         try:
             mpf.plot(kline, type=type, title=title, savefig=savePath)
             self.logger.info("Klines plotted")
@@ -189,15 +200,16 @@ class BinanceBot(Subscriber):
             self.logger.error("PlotKlines error: %s" % err)
             return None
 
+
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Binance bot configuration')
-    parser.add_argument('--broker', default='mosquitto')
-    parser.add_argument('--port', default=1883)
-    parser.add_argument('--topic', default='pair')
-    parser.add_argument('--client_id', default='binancebot')
-    parser.add_argument('--data_dir', default='./binance/data')
-    parser.add_argument('--time_limit', type=int, default=20)
+    parser = argparse.ArgumentParser(description="Binance bot configuration")
+    parser.add_argument("--broker", default="mosquitto")
+    parser.add_argument("--port", default=1883)
+    parser.add_argument("--topic", default="pair")
+    parser.add_argument("--client_id", default="binancebot")
+    parser.add_argument("--data_dir", default="./binance/data")
+    parser.add_argument("--time_limit", type=int, default=20)
     args = parser.parse_args()
 
     broker = args.broker
@@ -207,4 +219,11 @@ if __name__ == "__main__":
     data_dir = args.data_dir
     time_limit = args.time_limit
 
-    bbot = BinanceBot(broker=broker, port=port, topic=topic, client_id=client_id, dataDir=data_dir, timeLimit=time_limit)
+    bbot = BinanceBot(
+        broker=broker,
+        port=port,
+        topic=topic,
+        client_id=client_id,
+        dataDir=data_dir,
+        timeLimit=time_limit,
+    )
--- slack/slackbot.py
+++ slack/slackbot.py
@@ -10,15 +10,17 @@ from utils.mqtt import Subscriber, Publisher
 from utils.logger import get_logger
 
 
-parser = argparse.ArgumentParser(description='Slack bot configuration.')
-parser.add_argument('--broker', default='mosquitto')
-parser.add_argument('--port', type=int, default=1883)
-parser.add_argument('--topic', default='pair')
-parser.add_argument('--client_id', default='slackbot-pub')
+parser = argparse.ArgumentParser(description="Slack bot configuration.")
+parser.add_argument("--broker", default="mosquitto")
+parser.add_argument("--port", type=int, default=1883)
+parser.add_argument("--topic", default="pair")
+parser.add_argument("--client_id", default="slackbot-pub")
 args = parser.parse_args()
 
 app = Flask(__name__)
-slack_events_adapter = SlackEventAdapter(os.getenv("SLACK_EVENT_TOKEN"), "/slack/events", app)
+slack_events_adapter = SlackEventAdapter(
+    os.getenv("SLACK_EVENT_TOKEN"), "/slack/events", app
+)
 slack_web_client = WebClient(os.getenv("SLACK_TOKEN"))
 
 broker = args.broker
@@ -27,8 +29,8 @@ topic = args.topic
 client_id = args.client_id
 pub = Publisher(broker, port, client_id)
 
-logger = get_logger(name='SBOT')
-available_commands = ['chart', 'price', 'ping', 'info', 'help', 'start', 'stop']
+logger = get_logger(name="SBOT")
+available_commands = ["chart", "price", "ping", "info", "help", "start", "stop"]
 
 
 @slack_events_adapter.on("message")
@@ -39,61 +41,69 @@ def message(payload):
         textSlice = text.strip().split()
         channel = event.get("channel")
         userID = event.get("user")
-        
-        if event.get('bot_id'):
+
+        if event.get("bot_id"):
             return
 
         if textSlice[0] not in available_commands:
-            logger.info('Command not recognised')
+            logger.info("Command not recognised")
             return
-        
-        if textSlice[0] == 'help':
-            message = 'Available commands:\n' + '1. ping\n' +'2. chart (coinpair) (interval) (start time)\n' + '3. price (coinpair)\n' + '4. info (coinpair) (interval)'
-            slackMsg(channelID=channel, msg=message)            
 
-        if textSlice[0] == 'chart':
+        if textSlice[0] == "help":
+            message = (
+                "Available commands:\n"
+                + "1. ping\n"
+                + "2. chart (coinpair) (interval) (start time)\n"
+                + "3. price (coinpair)\n"
+                + "4. info (coinpair) (interval)"
+            )
+            slackMsg(channelID=channel, msg=message)
+
+        if textSlice[0] == "chart":
             handleChartPrice(command=textSlice, channel=channel)
 
-        if textSlice[0] == 'info':
+        if textSlice[0] == "info":
             handleInfo(command=textSlice, channel=channel)
 
-        if textSlice[0] == 'price':
+        if textSlice[0] == "price":
             handleCheckPrice(command=textSlice, channel=channel)
-        
-        if textSlice[0] == 'ping':
+
+        if textSlice[0] == "ping":
             handlePing(channel=channel)
 
-        if textSlice[0] == 'start':
+        if textSlice[0] == "start":
             handleStart(command=textSlice, channel=channel, userID=userID)
-        
-        if textSlice[0] == 'stop':
+
+        if textSlice[0] == "stop":
             handleStop(command=textSlice, channel=channel, userID=userID)
-    
+
     except Exception as err:
         logger.error("Slack OnMessage error: %s" % err)
 
+
 def handleStart(command, channel, userID):
     if len(command) != 3:
-        logger.info('Wrong syntax for starting price report')
-        message = 'Wrong syntax for starting price report, try:\nstart (coinpair) (report interval)'
+        logger.info("Wrong syntax for starting price report")
+        message = "Wrong syntax for starting price report, try:\nstart (coinpair) (report interval)"
         slackMsg(channelID=channel, msg=message)
-        return 
+        return
     coinPair = command[1].upper()
     interval = command[2]
     payload = {
         "type": "start",
         "coinPair": coinPair,
-        "interval": interval, 
+        "interval": interval,
         "userID": userID,
-        "channelID": channel
+        "channelID": channel,
     }
     payload = json.dumps(payload)
     pub.publish(topic=topic, message=payload)
 
+
 def handleStop(command, channel, userID):
     if len(command) != 2:
-        logger.info('Wrong syntax for stopping price report')
-        message = 'Wrong syntax for stopping price report, try:\nstop (coinpair)'
+        logger.info("Wrong syntax for stopping price report")
+        message = "Wrong syntax for stopping price report, try:\nstop (coinpair)"
         return
 
     coinPair = command[1].upper()
@@ -101,15 +111,16 @@ def handleStop(command, channel, userID):
         "type": "stop",
         "coinPair": coinPair,
         "userID": userID,
-        "channelID": channel
+        "channelID": channel,
     }
     payload = json.dumps(payload)
     pub.publish(topic=topic, message=payload)
 
+
 def handleChartPrice(command, channel):
     if len(command) != 4:
-        logger.info('Wrong syntax for get price chart')
-        message = 'Wrong syntax, try:\nchart (coinpair) (interval) (start time)'
+        logger.info("Wrong syntax for get price chart")
+        message = "Wrong syntax, try:\nchart (coinpair) (interval) (start time)"
         slackMsg(channelID=channel, msg=message)
         return
 
@@ -121,59 +132,53 @@ def handleChartPrice(command, channel):
         "coinPair": coinPair,
         "interval": interval,
         "startDate": startTime,
-        "channelID": channel
-    }    
+        "channelID": channel,
+    }
     payload = json.dumps(payload)
-    pub.publish(topic=topic, message=payload)     
+    pub.publish(topic=topic, message=payload)
+
 
 def handleCheckPrice(command, channel):
     if len(command) != 2:
-        logger.info('Wrong syntax to check average price')
-        message = 'Wrong syntax, try:\nprice (coinpair)'
+        logger.info("Wrong syntax to check average price")
+        message = "Wrong syntax, try:\nprice (coinpair)"
         slackMsg(channelID=channel, msg=message)
         return
-    
+
     coinPair = command[1].upper()
-    payload = {
-        "type": "price",
-        "coinPair": coinPair,
-        "channelID": channel
-    }
+    payload = {"type": "price", "coinPair": coinPair, "channelID": channel}
     payload = json.dumps(payload)
-    pub.publish(topic=topic, message=payload)     
+    pub.publish(topic=topic, message=payload)
+
 
 def handleInfo(command, channel):
     if len(command) != 3:
-        logger.info('Wrong syntax to check coinpair info')
-        message = 'Wrong syntax, try:\ninfo (coinpair) (interval)'
+        logger.info("Wrong syntax to check coinpair info")
+        message = "Wrong syntax, try:\ninfo (coinpair) (interval)"
         slackMsg(channelID=channel, msg=message)
         return
-    
+
     coinPair = command[1].upper()
     interval = command[2]
     payload = {
         "type": "info",
         "coinPair": coinPair,
         "interval": interval,
-        "channelID": channel
+        "channelID": channel,
     }
     payload = json.dumps(payload)
     pub.publish(topic=topic, message=payload)
 
+
 def handlePing(channel):
-    payload = {
-        "type": "ping",
-        "channelID": channel
-    }
+    payload = {"type": "ping", "channelID": channel}
     payload = json.dumps(payload)
     pub.publish(topic=topic, message=payload)
+
 
 def slackMsg(channelID, msg):
     try:
-        res = slack_web_client.chat_postMessage(
-            channel=channelID,
-            text=msg
-        )
+        res = slack_web_client.chat_postMessage(channel=channelID, text=msg)
         logger.info("Msg sent to %s:\n%s" % (channelID, msg))
     except Exception as err:
         logger.error("Slack send message error %s" % err)
@@ -181,4 +186,4 @@ def slackMsg(channelID, msg):
 
 if __name__ == "__main__":
 
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host="0.0.0.0", port=5001)
